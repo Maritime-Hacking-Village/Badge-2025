da34006e3882234bbd7bcd275e71d6746286fbbd
Nicholas Haltmeyer <nick@liberas.io>
2025-08-15 17:31:40 -0400

    fix flash script

--- firmware/firmware/flash.sh
+++ firmware/firmware/flash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -eux
 
-cargo build && sudo ~/.cargo/bin/probe-rs run --chip RP235x target/thumbv8m.main-non-eabihf/debug/mhv_dc33_fw
+cargo build --target thumbv8m.main-none-eabihf
+sudo ~/.cargo/bin/probe-rs run --chip RP235x target/thumbv8m.main-non-eabihf/debug/mhv_dc33_fw
 
